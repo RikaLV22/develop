9f53d6963d485868e032c4c78c96027b1c1f664d
RikaLV22 <kairi07041234@gmail.com>
2026-03-15 00:42:19 +0900

    家計簿を組織単位で参照できるようにした

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,66 +1,73 @@
 class TransactionsController < ApplicationController
-    before_action :set_transaction, only: [:show, :update, :destroy]
+  before_action :set_transaction, only: [:show, :update, :destroy]
 
-    def index
-        transactions = Transaction
-            .where(user_id: @current_user.id)
-            .order(date: :desc)
+  def index
+    transactions = Transaction
+      .where(organization_id: @current_user.organization_id)
+      .order(date: :desc)
+      .includes(:user)
 
-        render json: transactions
+    render json: transactions.map { |t| transaction_json(t) }
+  end
+
+  def show
+    render json: transaction_json(@transaction)
+  end
+
+  def create
+    transaction = Transaction.new(transaction_params)
+    transaction.user_id = @current_user.id
+    transaction.organization_id = @current_user.organization_id
+
+    if transaction.save
+      render json: transaction_json(transaction), status: :created
+    else
+      render json: { errors: transaction.errors.full_messages }, status: :unprocessable_entity
     end
+  end
 
-    def show
-        render json: @transaction
+  def update
+    if @transaction.update(transaction_params)
+      render json: transaction_json(@transaction), status: :ok
+    else
+      render json: { errors: @transaction.errors.full_messages }, status: :unprocessable_entity
     end
+  end
 
-    def create
-        transaction = Transaction.new(transaction_params)
-        transaction.user_id = @current_user.id
-        transaction.organization_id = @current_user.organization_id
+  def destroy
+    @transaction.destroy
+    render json: { message: '削除しました' }
+  end
 
-        if transaction.save
-            render json: transaction, status: :created
-        else
-            render json: { errors: transaction.errors.full_messages },
-                    status: :unprocessable_entity
-        end
+  private
+
+  def transaction_json(t)
+    t.as_json(
+      except: [:created_at, :updated_at, :organization_id, :user_id]
+    )
+    .merge(
+      user_name: t.user&.username || "不明"
+    )
+  end
+
+  def set_transaction
+    @transaction = Transaction.find_by(
+      id: params[:id],
+      user_id: @current_user.id
+    )
+    unless @transaction
+      render json: { error: '権限がありません' }, status: :forbidden
     end
+  end
 
-    def update
-        if @transaction.update(transaction_params)
-            render json: @transaction, status: :ok
-        else
-            render json: {errors: @transaction.errors.full_messages},
-                    status: :unprocessable_entity
-        end
-    end
-
-    def destroy
-        @transaction.destroy
-        render json: {message: '削除しました'}
-    end
-
-    private
-
-    def set_transaction
-        @transaction = Transaction.find_by(
-            id: params[:id],
-            user_id: @current_user.id
-        )
-
-        unless @transaction
-            render json: { error: '権限がありません' }, status: :forbidden
-        end
-    end
-
-    def transaction_params
-        params.require(:transaction).permit(
-            :transaction_type,
-            :category,
-            :amount,
-            :date,
-            :payment_method,
-            :card_number
-        )
-    end
+  def transaction_params
+    params.require(:transaction).permit(
+      :transaction_type,
+      :category,
+      :amount,
+      :date,
+      :payment_method,
+      :card_number
+    )
+  end
 end
